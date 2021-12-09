6ed4a2a0d9779c48679d12c75b8b1f4917aa4701
ChristianTovar <tovarchristian21@gmail.com>
2021-12-09 17:13:54 -0500

    Add day 05-I solution

---
+++ lib/day_05.ex
@@ -0,0 +1,30 @@
+defmodule Aoc2021.Day05 do
+  def part1(input) do
+    input
+    |> Enum.filter(&ignore_diagonals/1)
+    |> Enum.map(&set_lines/1)
+    |> store_coordinates()
+    |> Map.values()
+    |> Enum.map(&Map.values/1)
+    |> Enum.map(&Enum.filter(&1, fn val -> val > 1 end))
+    |> List.flatten()
+    |> Enum.count()
+  end
+
+  defp ignore_diagonals({x, _y1, x, _y2}), do: true
+  defp ignore_diagonals({_x1, y, _x2, y}), do: true
+  defp ignore_diagonals(_), do: false
+
+  defp set_lines({x, y1, x, y2}), do: for(y <- y1..y2, do: {x, y})
+  defp set_lines({x1, y, x2, y}), do: for(x <- x1..x2, do: {x, y})
+
+  defp store_coordinates(coordinates_list) do
+    Enum.reduce(coordinates_list, %{}, fn coordinates_line, map ->
+      Enum.reduce(coordinates_line, map, fn {x, y}, acc ->
+        Map.update(acc, x, %{y => 1}, fn sub_coordinate_map ->
+          Map.update(sub_coordinate_map, y, 1, &(&1 + 1))
+        end)
+      end)
+    end)
+  end
+end
---
+++ test/day_05_test.exs
@@ -0,0 +1,26 @@
+defmodule Aoc2021.Day05Test do
+  use ExUnit.Case
+
+  import Aoc2021.Day05
+
+  setup_all do
+    {:ok, input_string} = File.read("input/day_05.txt")
+
+    input =
+      input_string
+      |> String.replace(~r/[->]/, "")
+      |> String.split()
+      |> Enum.map(&String.split(&1, ","))
+      |> List.flatten()
+      |> Enum.map(&String.to_integer/1)
+      |> Enum.chunk_every(4)
+      |> Enum.map(&List.to_tuple/1)
+
+    {:ok, input: input}
+  end
+
+  test "part1", state do
+    expected_result = 6113
+    assert expected_result == part1(state.input)
+  end
+end
