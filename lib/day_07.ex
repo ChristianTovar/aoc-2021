a9f04e01dfe129f1dd4f3f8bb8a11b63be716824
ChristianTovar <tovarchristian21@gmail.com>
2021-12-11 21:02:33 -0500

    Add day 07-I solution

---
+++ lib/day_07.ex
@@ -0,0 +1,18 @@
+defmodule Aoc2021.Day07 do
+  def part1(input) do
+    size = Enum.count(input)
+    separating_position = median(size)
+
+    median =
+      input
+      |> Enum.sort()
+      |> Enum.at(separating_position)
+
+    input
+    |> Enum.map(&abs(&1 - median))
+    |> Enum.sum()
+  end
+
+  defp median(size) when rem(size, 2) == 0, do: div(size, 2)
+  defp median(size), do: div(size + 1, 2)
+end
---
+++ test/day_07_test.exs
@@ -0,0 +1,22 @@
+defmodule Aoc2021.Day07Test do
+  use ExUnit.Case
+
+  import Aoc2021.Day07
+
+  setup_all do
+    {:ok, input_string} = File.read("input/day_07.txt")
+
+    input =
+      input_string
+      |> String.replace(~r/[\n]/, "")
+      |> String.split(",")
+      |> Enum.map(&String.to_integer/1)
+
+    {:ok, input: input}
+  end
+
+  test "part1", state do
+    expected_result = 336_040
+    assert expected_result == part1(state.input)
+  end
+end
