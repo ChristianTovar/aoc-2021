fe6e89f1b7376acd00f321bbf23913d887df34d4
ChristianTovar <tovarchristian21@gmail.com>
2021-12-16 12:23:14 -0500

    Add day 10-II solution

--- lib/day_10.ex
+++ lib/day_10.ex
@@ -1,12 +1,25 @@
 defmodule Aoc2021.Day10 do
-  @scores %{")" => 3, "]" => 57, "}" => 1197, ">" => 25137}
+  @scores_1 %{")" => 3, "]" => 57, "}" => 1197, ">" => 25137}
+  @scores_2 %{"(" => 1, "[" => 2, "{" => 3, "<" => 4}
 
   def part1(input) do
     input
     |> Enum.reduce([], fn row, acc -> [check_row(row, []) | acc] end)
     |> Enum.reject(&(&1 == :ok))
-    |> Enum.map(&@scores[&1])
+    |> Enum.map(&@scores_1[&1])
     |> Enum.sum()
+  end
+
+  def part2(input) do
+    input
+    |> Enum.reduce([], fn row, acc -> [check_row(row, []) | acc] end)
+    |> Enum.reverse()
+    |> Enum.zip(input)
+    |> Enum.filter(fn {indicator, _row} -> indicator == :ok end)
+    |> Enum.map(fn {_indicator, row} -> complete_row(row, []) end)
+    |> Enum.map(&Enum.reduce(&1, 0, fn char, acc -> acc * 5 + @scores_2[char] end))
+    |> Enum.sort()
+    |> get_middle_value()
   end
 
   defp check_row([], _acc), do: :ok
@@ -16,4 +29,25 @@ defmodule Aoc2021.Day10 do
   defp check_row([">" | t1], ["<" | t2]), do: check_row(t1, t2)
   defp check_row(["]" | t1], ["[" | t2]), do: check_row(t1, t2)
   defp check_row([h | _t], _acc), do: h
+
+  defp complete_row([], acc), do: acc
+  defp complete_row([h | t], acc) when h in ~w([ {  ( <), do: complete_row(t, [h | acc])
+  defp complete_row(["}" | t1], ["{" | t2]), do: complete_row(t1, t2)
+  defp complete_row([")" | t1], ["(" | t2]), do: complete_row(t1, t2)
+  defp complete_row([">" | t1], ["<" | t2]), do: complete_row(t1, t2)
+  defp complete_row(["]" | t1], ["[" | t2]), do: complete_row(t1, t2)
+  defp complete_row(["{" | t], acc), do: complete_row(t, ["}" | acc])
+  defp complete_row(["(" | t], acc), do: complete_row(t, [")" | acc])
+  defp complete_row(["<" | t], acc), do: complete_row(t, [">" | acc])
+  defp complete_row(["[" | t], acc), do: complete_row(t, ["]" | acc])
+  defp complete_row([_h | t], acc), do: complete_row(t, acc)
+
+  defp get_middle_value(values) do
+    middle =
+      values
+      |> Enum.count()
+      |> div(2)
+
+    Enum.at(values, middle)
+  end
 end
--- test/day_10_test.exs
+++ test/day_10_test.exs
@@ -18,4 +18,9 @@ defmodule Aoc2021.Day10Test do
     expected_result = 392_043
     assert expected_result == part1(state.input)
   end
+
+  test "part2", state do
+    expected_result = 1_605_968_119
+    assert expected_result == part2(state.input)
+  end
 end
